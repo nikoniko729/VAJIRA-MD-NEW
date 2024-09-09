1020bb8da4724807ac18070b56624528cde7f162
nikoniko729 <pamidukeshara27@gmail.com>
2024-09-09 16:19:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=mJckjBoT#xXaVgws-D1wl3Qgt6PZ-NBZoX4I6xiAdxbgYzHOWEeI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=qENUTCQA#fwd9LAUxFuA8VB5PUtGCaR7VtTTHWYuKkvTc-Uzsrn8' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94704020146' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
